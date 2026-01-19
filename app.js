0204055079203f172b684ed1087c4d18f55a0cf2
Nicolas Gatien <nicolas.gatien6@gmail.com>
2026-01-19 17:42:02 -0500

    add page titles
    resolves #2

--- app.js
+++ app.js
@@ -9,14 +9,20 @@ searchButton.addEventListener('click', async (e) => {
     const response = await fetch(`https://handmade-api-34lhk.ondigitalocean.app/search?${params}`, {
         method: "GET"
     });
+    
 
     const data = await response.json()
     const results = data['results']
 
+
     searchResultsList.innerHTML = "<ol></ol>"
 
     for (const element of results) {
-        searchResultsList.innerHTML += `<li><a href="${element[0]}">${element[0]}</a></li>`
+        let title = element[1]['title']
+        if (title == undefined) {
+            title = element[0]
+        }
+        searchResultsList.innerHTML += `<li><a href="${element[0]}">${title}</a></li>`
     }
 
     console.log(data['results'])
