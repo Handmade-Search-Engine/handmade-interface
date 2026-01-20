cf3f2227be6b555cc9da50af61e50333f9b89396
Nicolas Gatien <nicolas.gatien6@gmail.com>
2026-01-20 11:16:23 -0500

    add hostnames

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const searchButton = document.querySelector('button')
 const searchInput = document.querySelector('input')
 const searchResultsList = document.querySelector('#search-results')
 
+let api = "https://handmade-api-34lhk.ondigitalocean.app"
+if (location.hostname == "localhost" || location.hostname == "127.0.0.1") {
+    api = "http://127.0.0.1:5000"
+}
+
 searchButton.addEventListener('click', async (e) => {
     searchResultsList.innerHTML = "<p>Searching...</p>"
     const params = new URLSearchParams();
     params.append("query", searchInput.value);
-    const response = await fetch(`https://handmade-api-34lhk.ondigitalocean.app/search?${params}`, {
+    const response = await fetch(`${api}/search?${params}`, {
         method: "GET"
     });
     
@@ -15,14 +20,33 @@ searchButton.addEventListener('click', async (e) => {
     const results = data['results']
 
 
-    searchResultsList.innerHTML = "<ol></ol>"
+    searchResultsList.innerHTML = ""
 
     for (const element of results) {
         let title = element[1]['title']
         if (title == undefined) {
             title = element[0]
         }
-        searchResultsList.innerHTML += `<li><a href="${element[0]}">${title}</a></li>`
+        let hostname = element[1]['hostname']
+        let url = element[0]
+
+        let hostnameElement = document.createElement("p")
+        let hostnameTextNode = document.createTextNode(hostname)
+        hostnameElement.appendChild(hostnameTextNode)
+        hostnameElement.className = "result-hostname"
+
+        let urlElement = document.createElement("a")
+        let urlTextNode = document.createTextNode(title)
+        urlElement.appendChild(urlTextNode)
+        urlElement.className = "result-url"
+        urlElement.href = url
+
+        let elementContainer = document.createElement("div")
+        elementContainer.appendChild(hostnameElement)
+        elementContainer.appendChild(urlElement)
+        elementContainer.className = "search-result"
+
+        searchResultsList.appendChild(elementContainer)
     }
 
     console.log(data['results'])
